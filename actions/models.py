e23d93da9f60fb3d8b50784f0943d3c90531e3ee
GalRavil <gal.ravil93@gmail.com>
2016-11-29 19:50:50 +0300

    + Action model

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    user = models.ForeignKey(User, related_name='actions',
+                             db_index=True)
+    verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj')
+    # primary key of related object
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    # doesn't create any field in DB
+    target = GenericForeignKey('target_ct', 'target_id')
+    created = models.DateTimeField(auto_now_add=True,
+                                   db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
